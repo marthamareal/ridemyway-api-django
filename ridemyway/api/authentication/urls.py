b3c3cd7a4f3e48b896c2e9424175910c8a171def
marthamareal <marthamareal@gmail.com>
2019-09-23 01:17:53 +0300

    add api documentation

--- ridemyway/api/authentication/urls.py
+++ ridemyway/api/authentication/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('users/', views.UserView.as_view(), name='create_and_list_users'),
+    path('users/login/', views.UserLoginView.as_view(), name='user_login'),
+    path('users/activate/<str:token>/', views.ActivateUserView.as_view(), name='user_activate'),
     path('users/<pk>/', views.SingleUserView.as_view(), name='single_user'),
-    path('user/login/', views.UserLoginView.as_view(), name='user_login'),
-    path('user/activate/<str:token>/', views.ActivateUserView.as_view(), name='user_activate')
-
+    path('users/', views.UserView.as_view(), name='create_and_list_users'),
 ]
